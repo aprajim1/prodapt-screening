ca32abfe5a0471ec310caf3cd32aa94341a19a21
aprajim1 <67955841+aprajim1@users.noreply.github.com>
2025-08-17 14:55:56 +0530

    Update DemoApplicationTests.java

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -20,59 +20,6 @@ class DemoApplicationTests {
 	void contextLoads() {
 	}
 
-	@Test
-		void testRemove_commonWord() throws Exception {
-			mockMvc.perform(get("/remove").param("string", "Aprajita"))
-					.andExpect(status().isOk())
-					.andExpect(content().string("prajit"));
-		}
 
-	@Test
-		void testRemove_twoCharacters() throws Exception {
-			mockMvc.perform(get("/remove").param("string", "AB"))
-					.andExpect(status().isOk())
-					.andExpect(content().string(""));
-		}
-
-	@Test
-		void testRemove_emptyString() throws Exception {
-			mockMvc.perform(get("/remove").param("string", ""))
-					.andExpect(status().isBadRequest());
-		}
-
-	@Test
-		void testRemove_oneCharacter() throws Exception {
-			mockMvc.perform(get("/remove").param("string", "A"))
-					.andExpect(status().isBadRequest());
-		}
-
-	@Test
-		void testRemove_threeCharacters() throws Exception {
-			mockMvc.perform(get("/remove").param("string", "XYZ"))
-					.andExpect(status().isOk())
-					.andExpect(content().string("Y"));
-		}
-
-		// --- Strings with numbers and special characters ---
-	@Test
-		void testRemove_numbers() throws Exception {
-			mockMvc.perform(get("/remove").param("string", "12345"))
-					.andExpect(status().isOk())
-					.andExpect(content().string("234"));
-		}
-
-	@Test
-		void testRemove_specialCharacters() throws Exception {
-			mockMvc.perform(get("/remove").param("string", "@Hello!"))
-					.andExpect(status().isOk())
-					.andExpect(content().string("Hell"));
-		}
-
-	@Test
-		void testRemove_symbolsOnly() throws Exception {
-			mockMvc.perform(get("/remove").param("string", "$%&*"))
-					.andExpect(status().isOk())
-					.andExpect(content().string("%&"));
-		}
 	}
 
